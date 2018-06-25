d8d966c767f0dfc5436e87800d39b8416102f75e
James Ferguson <ncjamesferguson@gmail.com>
2018-06-25 22:37:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
       client_id    = ENV["GITHUB_CLIENT_ID"]
       redirect_uri = CGI.escape("http://localhost:3000/auth")
+      state = ENV['GITHUB_STATE_CODE']
       github_url   = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=repo"
       redirect_to github_url unless logged_in?
     end
